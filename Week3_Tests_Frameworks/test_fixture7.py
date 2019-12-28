0851bc7a6db69fcb29b9ad99903b85799fa8c39d
DENYS BILOHRYVYI <belogryvyi@gmail.com>
2019-12-28 16:47:03 +0200

    [Add] - Added Fixture 7 - Parameterization. Using @pytest.mark.parametrize decorator

---
+++ Week3_Tests_Frameworks/test_fixture7.py
@@ -0,0 +1,21 @@
+'''
+Fixture 7 - Parameterization. To run the same test several times with different input parameters using
+                                                                    @pytest.mark.parametrize decorator.
+'''
+import pytest, time
+from selenium import webdriver
+
+@pytest.fixture(scope="function")
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+
+@pytest.mark.parametrize('language', ["ru", "en-gb"])
+def test_guest_should_see_login_link(browser, language):
+    link = f"http://selenium1py.pythonanywhere.com/{language}/"
+    browser.get(link)
+    browser.find_element_by_css_selector("#login_link")
+    time.sleep(5)
